f993b6647d0aad94e6ecb48b5247b988fbaba7ea
雅韵 <56445536+Eleg-i@users.noreply.github.com>
2023-04-18 17:07:47 +0800

    完成 bin

--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,21 @@
 #!/usr/bin/env node
-
-const { execSync } = require('child_process'),
+const { spawnSync } = require('child_process'),
       systemDialog = require('@cailiao/system-dialog'),
-      args = process.argv.slice(2),
-      result = execSync('git diff --name-only ORIG_HEAD HEAD', { cwd: __dirname, encoding: 'utf-8' }),
-      files = result.split('\n')
+      args = process.argv.slice(2)
 var message = '项目依赖已修改！请重新安装依赖： npm run install',
-    customFiles = ['package.json', 'package-lock.json']
-
-files.forEach(file => file.replace('\r', ''))
+    customFiles = ['package.json', 'package-lock.json'],
+    status
 
 args.forEach(arg => {
-  message = arg.match(/^-message=(\S+)$/)?.[1] ?? message
-  customFiles = arg.match(/^-files=(\S+)$/)?.[1]?.split(',') ?? customFiles
+  message = arg.match(/^-message=(.+)$/)?.[1] ?? message
+  customFiles = arg.match(/^-files=(.+)$/)?.[1]?.split(',') ?? customFiles
 })
 
-customFiles.some(customFile => {
-  if (files.find(file => new RegExp(customFile).test(file))) {
-    systemDialog({ info: '', text: message })
+try {
+  ({ status } = spawnSync('git', ['diff', 'HEAD^', 'HEAD', '--exit-code', '--', ...customFiles], {
+    cwd: process.cwd(),
+    encoding: 'utf-8'
+  }))
+} catch {}
 
-    return true
-  }
-})
+if (status) systemDialog({ info: '', text: message })
